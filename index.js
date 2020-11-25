a1353a97504f1641d59f40184ae74ee28fba4c05
thankus <1056598469@qq.com>
2020-11-25 22:52:05 +0800

    Delete index.js

--- index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import List from './list';
-
-ReactDOM.render(
-  <List />,
-  document.getElementById('root')
-);
