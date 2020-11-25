1b73d607345d631c5cfeef05c5a25ceadec4ec94
thankus <1056598469@qq.com>
2020-11-25 22:51:07 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import List from './list';
+
+ReactDOM.render(
+  <List />,
+  document.getElementById('root')
+);
